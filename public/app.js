4c163f9e87f42d0a2fa80244f542bbbdb8ebcf9b
Miguel Guerrero <mgangsal@outlook.com>
2021-06-17 11:57:29 -0500

    Flipping and cards updated.

--- public/app.js
+++ public/app.js
@@ -3,6 +3,8 @@ const button = document.querySelector("button");
 const playerNumber = document.querySelector("#num_players");
 const cardNumber = document.querySelector("#num_cards");
 
+const flippedReferences = [];
+
 button.addEventListener("click", (e) => {
 	e.preventDefault();
 
@@ -21,6 +23,21 @@ button.addEventListener("click", (e) => {
 		.then(onFinishedFetching);
 });
 
+function flippedCard(element) {
+	if (element.classList.contains("flipped")) {
+		element.classList.remove("flipped");
+	} else {
+		element.classList.add("flipped");
+	}
+}
+
+function cleanCards() {
+	const allCards = document.querySelectorAll(".card");
+	allCards.forEach((card) =>
+		card.removeEventListener(card, flippedReferences.shift())
+	);
+}
+
 function mapCards(card) {
 	const number = card.slice(0, -1);
 	const symbol = card.slice(-1);
@@ -29,6 +46,7 @@ function mapCards(card) {
 
 function onFinishedFetching(res) {
 	console.log(res);
+	cleanCards();
 	const { deck, hands } = res;
 
 	const deckElements = deck.map(mapCards);
@@ -39,38 +57,46 @@ function onFinishedFetching(res) {
 		container.innerHTML += `<h2>Player ${index + 1}</h2>`;
 		container.innerHTML += newHand.join("\n");
 	});
+
+	const allCards = document.querySelectorAll(".card");
+	allCards.forEach((card) => {
+		const flipFunction = () => flippedCard(card);
+		flippedReferences.push(flipFunction);
+		card.addEventListener("click", flipFunction);
+	});
 }
 
 function createCard(symbol, number) {
-	if (number === "A") {
-		console.log("Ocurred");
-	}
-	// number = number === "A" ? 1 : number;
 	const isNumber = !isNaN(number) || number === "A";
 	const fixedSize = number === "A" ? 1 : number;
 	return `
-	<div class="card ${symbol}" number="${number}">
-		<div class="card-corner top-left">
-			<div>${number}</div>
-			<div>${symbol}</div>
+	<div class="card card-${symbol}" number="${number}">
+		<div class="card-front">
+			<div class="card-corner top-left">
+				<div>${number}</div>
+				<div>${symbol}</div>
+			</div>
+			<div class="symbols">
+				${
+					isNumber
+						? `${new Array(parseInt(fixedSize))
+								.fill(symbol)
+								.map(
+									(cardSymbol) => `
+						<div>${cardSymbol}</div>
+					`
+								)
+								.join("")}`
+						: ""
+				}
+			</div>
+			<div class="card-corner bottom-right">
+				<div>${number}</div>
+				<div>${symbol}</div>
+			</div>
 		</div>
-		<div class="symbols">
-			${
-				isNumber
-					? `${new Array(parseInt(fixedSize))
-							.fill(symbol)
-							.map(
-								(cardSymbol) => `
-					<div>${cardSymbol}</div>
-				`
-							)
-							.join("")}`
-					: ""
-			}
-		</div>
-		<div class="card-corner bottom-right">
-			<div>${number}</div>
-			<div>${symbol}</div>
+		<div class="card-back">
+			<p>Back of the card</p>
 		</div>
 	</div>`;
 }
